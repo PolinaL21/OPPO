f5a49df466cbb981c4fb4cc19a5069426484cf8b
PolinaL21 <119047763+PolinaL21@users.noreply.github.com>
2022-11-28 15:23:14 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-double my_pow(double n,unsigned int deg){
-  double res=1;
-  for (int i=0; i<deg;i++)
-    res*=n;
-  return res;}
+double my_pow(double num, int deg) {
+    double result = 1;
+
+    if(deg < 0) {
+        deg = -deg;
+        for(long i = 0; i < deg; i++) {
+            result *= num;
+        }
+
+        return 1 / result;
+    }
+    else {
+        for (long i = 0; i < deg; i++) {
+            result *= num;
+        }
+
+        return result;
+    }
+}
