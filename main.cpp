55e07647f03a5fb4329ce6cdc60905d787335d7d
PolinaL21 <119047763+PolinaL21@users.noreply.github.com>
2022-11-28 15:00:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 double my_powup(double num, int deg) {
     double result = 1;
-
     if(deg < 0) {
         deg = -deg;
         for(long i = 0; i < deg; i++) {
             result *= num;
         }
-
         return 1 / result;
     }
     else {
         for (long i = 0; i < deg; i++) {
             result *= num;
         }
-
         return result;
     }
 }
