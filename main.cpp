a079f5351e4b9046c4babe8175c8a19f4c4d6e71
PolinaL21 <119047763+PolinaL21@users.noreply.github.com>
2022-11-28 14:39:31 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-double my_pow(double n,unsigned int k){
-  double s=1;
-  for (int i=1; i<=k;++i)
-    s*=n;
-  return s;
+long powerFast(long num, long deg) {
+    long result = 1;
+    while(deg) {
+        if (deg % 2 == 0) {
+            deg /= 2;
+            num *= num;
+        }
+        else {
+            deg--;
+            result *= num;
+        }
+    }
+    return result;
 }
